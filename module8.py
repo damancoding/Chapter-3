efe3116d50ad1d6fb3e5f03650bde547086eb75a
Amanda M <156499790+damancoding@users.noreply.github.com>
2024-03-03 10:01:18 -0500

    Add files via upload

---
+++ module8.py
@@ -0,0 +1,23 @@
+# Amanda M
+# 1/30/2024
+# module 8 in python
+
+
+def classA():
+    classATickets = int(input("How many tickets were sold for Class A? "))
+    classASales = classATickets * 15
+    classATotal = print("Class A sold", classATickets, "tickets and earned $",classASales, "dollars.")
+
+def classB():
+    classBTickets = int(input("How many tickets were sold for Class B? "))
+    classBSales = classBTickets * 12
+    classBTotal = print("Class B sold", classBTickets, "tickets and earned $",classBSales, "dollars.")
+
+def classC():
+    classCTickets = int(input("How many tickets were sold for Class C? "))
+    classCSales = classCTickets * 9
+    classCTotal = print("Class C sold", classCTickets, "tickets and earned $",classCSales, "dollars.")
+
+classA()
+classB()
+classC()
