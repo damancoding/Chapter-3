c1b152534e4ba3ca4b60ee4c1da4aa25c9e6c08c
Amanda M <156499790+damancoding@users.noreply.github.com>
2024-03-03 10:03:04 -0500

    Add files via upload

---
+++ Exercise 3_code.py
@@ -0,0 +1,10 @@
+# Amanda M
+# 1/29/2024
+# Code for Exercise 3
+
+def main():
+    houseCost = float(input("How much would it cost to replace the structure of your home? "))
+    insureCost = houseCost * .8
+    print("Your minimum insurance amount would cost", insureCost, "dollars.")
+
+main()
